2e1e524b094d1728d088985a16d5b5f7fb5146d1
Okkun555 <y.okuno.btm@gmail.com>
2023-05-07 22:21:13 +0900

    refactor: 💡 storybookの記述方法を修正

--- src/ui/components/SubmitButton/SubmitButton.stories.tsx
+++ src/ui/components/SubmitButton/SubmitButton.stories.tsx
@@ -1,15 +1,20 @@
-import { ComponentMeta } from "@storybook/react";
 import SubmitButton from ".";
 
 export default {
   title: "SubmitButton",
   component: SubmitButton,
-} as ComponentMeta<typeof SubmitButton>;
-
-export const Disabled = () => {
-  return <SubmitButton text="送信" disabled={true} />;
 };
 
-export const Enabled = () => {
-  return <SubmitButton text="送信" disabled={false} />;
+export const Disabled = {
+  args: {
+    text: "送信",
+    disabled: true,
+  },
+};
+
+export const Enabled = {
+  args: {
+    text: "送信",
+    disabled: false,
+  },
 };
