b139d700d758ca1bcccca1245c64c0ce9667aacf
Okkun555 <y.okuno.btm@gmail.com>
2023-05-23 23:33:27 +0900

    feat: 🎸 inputフォームにtype属性を指定できるように改修

--- src/ui/components/Input/input.stories.tsx
+++ src/ui/components/Input/input.stories.tsx
@@ -1,15 +1,40 @@
 import { useForm } from "react-hook-form";
 import Input from "./input";
+import { StoryFn } from "@storybook/react";
 
 export default {
   title: "ui/Input",
   component: Input,
 };
 
-const Template = () => {
+const Template: StoryFn = (args) => {
   const { register } = useForm();
 
-  return <Input label={"sample"} register={register} required={true} />;
+  return (
+    <Input
+      type={args.type}
+      label={"sample"}
+      register={register}
+      required={true}
+      placeholder={args.placeholder}
+    />
+  );
 };
 
-export const SimpleTextInput = Template.bind({});
+export const TextInput = Template.bind({});
+TextInput.args = {
+  type: "text",
+  placeholder: "sample text input placeholder",
+};
+
+export const NumberInput = Template.bind({});
+NumberInput.args = {
+  type: "number",
+  placeholder: "sample number input placeholder",
+};
+
+export const PasswordInput = Template.bind({});
+PasswordInput.args = {
+  type: "password",
+  placeholder: "sample password input placeholder",
+};
--- src/ui/components/Input/input.tsx
+++ src/ui/components/Input/input.tsx
@@ -1,6 +1,10 @@
+import React from "react";
 import { FieldValues, Path, UseFormRegister } from "react-hook-form";
 
+type InputType = "text" | "email" | "password" | "number";
+
 type InputProps<T extends FieldValues> = {
+  type: InputType;
   label: Path<T>;
   register: UseFormRegister<T>;
   required: boolean;
@@ -8,12 +12,12 @@ type InputProps<T extends FieldValues> = {
 };
 
 const Input = <T extends FieldValues>(props: InputProps<T>) => {
-  const { label, register, required, placeholder = "" } = props;
+  const { type, label, register, required, placeholder = "" } = props;
 
   return (
     <input
       {...register(label)}
-      type="text"
+      type={type}
       className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-100 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
       required={required}
       placeholder={placeholder}
